8a666a3d0030a5186030ef6f7d39120d797a0e7b
Sean Fioritto <sean@planningforaliens.com>
2023-04-08 23:15:09 -0500

    Add highlights.

--- app/models.py
+++ app/models.py
@@ -5,10 +5,21 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.Text, nullable=False)
     post_type = db.Column(db.String(10), nullable=False)
+    highlights = db.relationship('Highlight', backref='post', lazy=True)
     notes = db.relationship('Note', backref='post', lazy=True)
+
+
+class Highlight(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    start_pos = db.Column(db.Integer, nullable=False)
+    end_pos = db.Column(db.Integer, nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+    notes = db.relationship('Note', backref='highlight', lazy=True)
 
 
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.Text, nullable=False)
-    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+    highlight_id = db.Column(
+        db.Integer, db.ForeignKey('highlight.id'), nullable=True)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=True)
